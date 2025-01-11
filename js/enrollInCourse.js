e46805685e38ff1d8f02917f74931ee394afe526
shahadot99999 <shahadot99999@gmail.com>
2025-01-11 08:45:08 +0600

    E-Learning Platform

---
+++ js/enrollInCourse.js
@@ -0,0 +1,7 @@
+ // Enroll in Course Function
+ function enrollInCourse(courseId) {
+    // Display an alert for now
+    alert("Wellcome ...You have enrolled in Course " + courseId + "!");
+
+    
+  }
